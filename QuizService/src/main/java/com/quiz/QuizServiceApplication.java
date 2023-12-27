4dea234aececec65944f8b4031b496a8f4e825ad
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2023-12-27 16:18:38 +0530

    quiz service completed

--- QuizService/src/main/java/com/quiz/QuizServiceApplication.java
+++ QuizService/src/main/java/com/quiz/QuizServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class QuizServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(QuizServiceApplication.class, args);
 	}
 
 }
---
+++ QuizService/src/main/java/com/quiz/controllers/QuizController.java
@@ -0,0 +1,42 @@
+package com.quiz.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.entities.Quiz;
+import com.quiz.services.QuizService;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+	
+	private QuizService quizSer;
+
+	public QuizController(QuizService quizSer) {
+		super();
+		this.quizSer = quizSer;
+	}
+	
+	@PostMapping
+	public Quiz create(@RequestBody Quiz quiz) {
+		return quizSer.add(quiz);
+	}
+	
+	@GetMapping
+	public List<Quiz> getAll() {
+		return quizSer.getAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Quiz get(@PathVariable Long id) {
+		return  quizSer.get(id);
+	}
+	
+
+}
